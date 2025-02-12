3e48fbd1c4b85bcfbca7f31801ead1bf52349ad3
karry <carryzho@outlook.com>
2025-02-12 14:07:25 +0800

    添加MiniExcel第三方库

---
+++ CSharpEssential/Enumerable.Where/Program.cs
@@ -0,0 +1,18 @@
+﻿using MiniExcelLibs;
+
+namespace Enumerable.Where
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? testDataFilePath = @"D:\Code\TestData\TestData.xlsx";
+            var sheetNames = MiniExcel.GetSheetNames(testDataFilePath);
+
+            foreach (var sheetName in sheetNames)
+            {
+                Console.WriteLine(sheetName);
+            }
+        }
+    }
+}
