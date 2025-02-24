51d1853dd10367a7000104e364d6a923ca5b4f92
karry <carryzho@outlook.com>
2025-02-24 23:05:05 +0800

    Dynamic

--- CSharpEssential/Enumerable.Where/Program.cs
+++ CSharpEssential/Enumerable.Where/Program.cs
@@ -5,33 +5,27 @@ namespace Enumerable.Where
 {
     internal class Program
     {
-        private static readonly string 交易代码;
-
         static void Main(string[] args)
         {
             string? testDataFilePath = @"D:\Test\RawData.xlsx";
 
             //首行作为标题行，读取TestData.xlsx文件“分表”工作簿
             //统计“上市地点”为“上海”，“发行人企业性质”为“民营企业”的数量并逐条输出
-            var rows = MiniExcel.Query(testDataFilePath, sheetName: "分表", useHeaderRow: true).ToList();
+            var rows = MiniExcel.Query(testDataFilePath, sheetName: "分表", useHeaderRow: true);
             var filteredRows = rows.Where(row => row.上市地点 == "上海" && row.发行人企业性质 == "民营企业");
             Console.WriteLine(@"“上市地点”为“上海”，“发行人企业性质”为“民营企业”共有 {0} 家", filteredRows.Count());
 
             foreach (var row in filteredRows)
             {
-                var dictRow = row as IDictionary<string, object>;
-
-                Console.WriteLine("{0,-12}{1,-8}{2,-8}{3,-8}{4,-14}{5,-8}{6,-8}{7,-8}{8,-8}{9,-8}",
-                    dictRow["交易代码"],
-                    dictRow["发行起始日"],
-                    dictRow["发行规模(亿)"],
-                    dictRow["票面利率(%)"],
-                    dictRow["上市日期"],
-                    dictRow["上市地点"],
-                    dictRow["利率类型"],
-                    dictRow["发行人简称"],
-                    dictRow["发行人企业性质"],
-                    dictRow["发行人省份"]
+                Console.WriteLine("{0,-12}{1,-12}{2,-12}{3,-12}{4,-12}{5,-12}{6,-12}{7,-12}",
+                    row.交易代码,
+                    row.发行起始日,
+                    row.上市日期,
+                    row.上市地点,
+                    row.利率类型,
+                    row.发行人简称,
+                    row.发行人企业性质,
+                    row.发行人省份
                     );
             }
         }
---
+++ CSharpEssential/Keywords.Dynamic/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Dynamic;
+namespace Keywords.Dynamic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic person = 10;    //person在运行时被解析为Int32
+            Console.WriteLine(person.GetType());
+
+            person.Name = "Alice";  //Int32没有定义Name、Age属性，且不支持动态添加属性，所以会抛出运行时异常
+            person.Age = 21;
+            Console.WriteLine(person.Age);
+        }
+    }
+}
