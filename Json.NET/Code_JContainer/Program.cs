7e834ee1185f8a3699cb9cd0e31fcc26fa633497
karry <carryzho@outlook.com>
2025-07-21 11:36:40 +0800

    Add Code_JObject

--- Json.NET/Code_JContainer/Program.cs
+++ Json.NET/Code_JContainer/Program.cs
@@ -13,6 +13,27 @@ namespace Code_JContainer
             JContainer jct = (JContainer)JToken.Parse(jsonContent);
 
             Console.WriteLine($"Children tokens of the jct:{jct.Count}");
+
+            jct.Merge(new JProperty("Timi", "Gaming now"));
+
+            Console.WriteLine($"Children tokens of the JContainer after merging a JProperty:{jct.Count}");
+
+            JObject job = new JObject
+            {
+                { "Name", "Timi" }
+            };
+
+            jct.Merge(job);
+            Console.WriteLine($"Children tokens of the JContainer after merging a JObject that has an existing property name:{jct.Count}");
+
+            JObject jbt = new JObject
+            {
+                { "Timi", "Gaming now" }
+            };
+
+            jct.Merge(jbt);
+            Console.WriteLine($"Children tokens of the JContainer after merging a JObject that has no existing property name:{jct.Count}");
+
         }
     }
 }
---
+++ Json.NET/Code_JObject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_JObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
