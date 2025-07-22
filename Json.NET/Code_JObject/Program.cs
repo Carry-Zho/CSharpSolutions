34356576ece6f8fd6604ec04e9f27074ac64dee1
karry <carryzho@outlook.com>
2025-07-22 15:01:50 +0800

    Close JProperty

--- Json.NET/Code_JObject/Program.cs
+++ Json.NET/Code_JObject/Program.cs
@@ -1,10 +1,88 @@
-﻿namespace Code_JObject
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Code_JObject
 {
     internal class Program
     {
+        #region cas1
+        //static void Main(string[] args)
+        //{
+        //    JObject joa = new JObject();
+        //    joa.Add("Timi", "Gaming now");
+
+        //    var job = new JObject(joa);
+
+        //    JObject joc = new()
+        //    {
+        //        { "天成", "Gaming now" } // Adding a property with a key-value pair
+        //    };
+
+
+        //    JObject jod = new()
+        //    {
+        //        ["雷火"] = "Gaming now" // Adding a property using indexer syntax
+        //    };
+
+        //}
+        #endregion
+        //#region case2
+        //static void Main(string[] args)
+        //{
+        //    JObject job = new JObject
+        //    {
+        //        ["天成"] = "Gaming now",
+        //        ["雷火"] = "Gaming tomorrow"
+        //    };
+        //    var result1 = job.GetValue("天成");
+        //    Console.WriteLine(result1!.Type);
+        //    Console.WriteLine(result1.GetType());
+
+        //    if(!job.TryGetValue("天雷", out result1))
+        //        Console.WriteLine("天雷属性不存在");
+
+        //    var result2 = job.GetValue("天雷"); // Attempting to get a value that does not exist
+        //    Console.WriteLine(result2!.Type);   //访问null值时会抛出异常
+        //    Console.WriteLine(result2.GetType());//访问null值时会抛出异常
+
+
+        //}
+
+        //#endregion
+
+        #region case3
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            JObject job = new JObject
+            {
+                ["天成"] = "Gaming now",
+                ["雷火"] = "Gaming tomorrow"
+            };
+
+            var result = job.Property("天成");
+
+            Console.WriteLine(result!.Type);
+            Console.WriteLine(result!.GetType());
+            Console.WriteLine($"属性名:{result.Name},属性值:{result.Value}");
+
+            result = job.Property("天雷"); // Attempting to get a property that does not exist
+            if (result == null)
+            {
+                Console.WriteLine("天雷属性不存在");
+            }
+
+            foreach (var item in job.Properties())
+            {
+                Console.WriteLine($"属性名:{item.Name},属性值:{item.Value}");
+            }
+
+            foreach (var item in job.PropertyValues())
+            {
+                Console.WriteLine(item);
+            }
+
         }
+        #endregion
     }
 }
---
+++ Json.NET/Code_JProperty/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Code_JProperty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject job = new() { 
+                new JProperty("name", "周鸣"),
+                new JProperty("age", 22),
+                new JProperty("isEmployed", true),
+            };
+            // 输出 JSON 字符串
+            Console.WriteLine(job.ToString());
+            Console.WriteLine("====================================");
+
+            JProperty newProperty = new JProperty("city", new[]{ "c1","c2","c3"});
+            job.Add(newProperty);
+
+            // 输出更新后的 JSON 字符串
+            Console.WriteLine(job.ToString());
+
+            Console.WriteLine("====================================");
+            // 遍历 JProperty
+            foreach (var property in job.Properties())
+            {
+                Console.WriteLine($"Name: {property.Name}, Value: {property.Value}");
+                Console.WriteLine("====================================");
+            }
+
+        }
+    }
+}
