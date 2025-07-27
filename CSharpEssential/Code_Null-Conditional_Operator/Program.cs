e8d5fb390dff6494056a561e54cb1f3be88f1ce3
karry <carryzho@outlook.com>
2025-07-27 22:41:49 +0800

    Add JSONPath Test01

---
+++ CSharpEssential/Code_Null-Conditional_Operator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Code_Null_Conditional_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string token = "Hello world.";
+
+            if (token?.ToString().StartsWith("H") ?? false)
+            {
+                Console.WriteLine("token is not null and starts with H.");
+            }
+            else 
+            {
+                Console.WriteLine("token is null or does not start with H.");
+            }
+        }
+    }
+}
--- Json.NET/Code_TestJSONPath/Program.cs
+++ Json.NET/Code_TestJSONPath/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System.Linq;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Code_TestJSONPath
@@ -10,8 +12,22 @@ namespace Code_TestJSONPath
             string jsonContent = File.ReadAllText("JSON.json");
 
             JArray jar = JsonConvert.DeserializeObject<JArray>(jsonContent)!;
-            //
+            
+            //列举具有“Name”属性的人员
+            string jsonPath = "$..[?(@.Name)]";
 
+            //var results = ((JContainer)jar).Descendants()
+            //    .OfType<JObject>()
+            //    .Where(obj => obj["Name"]?.ToString().StartsWith("张") ?? false);
+
+            var results = from token in jar.Descendants()
+                          where token is JObject job && (job["Name"]?.ToString().StartsWith("张") ?? false)
+                          select token;
+
+            foreach (var token in results)
+            {
+                Console.WriteLine(token);
+            }
         }
     }
 }
