581055088c52f5cd6233d6ec65d757f7c8fc54eb
威振 周 <carryzho@outlook.com>
2026-07-02 15:10:04 +0800

    Hello

---
+++ CSharpEssential/Test/HelloSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Sharp!");
+        }
+    }
+}
